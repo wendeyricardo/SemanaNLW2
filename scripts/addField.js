0968f436cd6eb602900566096540adbe7910aad9
wendey <wendeyricardo@gmail.com>
2020-08-12 00:24:36 -0300

    add o javascript no projeto

---
+++ scripts/addField.js
@@ -0,0 +1,22 @@
+//procurar o botão
+document.querySelector("#add-time").addEventListener('click', cloneField) //quando clicar no botao
+
+
+//executar uma acao
+function cloneField() {
+    //duplicar os campos.que campos?
+    const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true)
+
+    //pegar os campos. que campos?
+    const fields = newFieldContainer.querySelectorAll('input')
+
+    //para cada campo, limpar
+    fields.forEach(function(field){
+        //pegar o field do momento e limpa ele
+        field.value=""
+
+    })
+
+    //colocar na pagina: onde?
+    document.querySelector('#schedule-items').appendChild(newFieldContainer)
+}
