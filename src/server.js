85a6066ac426d0dd5918566c25e818ab06f34e60
wendey <wendeyricardo@gmail.com>
2020-08-16 17:32:45 -0300

    organizando o server.js e adicionando o nunjucks ao projeto

--- src/server.js
+++ src/server.js
@@ -3,46 +3,53 @@ const proffys = [
         name: "Diego Fernandes",
         avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
         whatsapp: "0000000",
-        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>
-    Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. 
-    Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
-    subject: "Química",
+        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões",
+        subject: "Química",
         cost: "20",
         weekday: [0],
         time_from: [720],
         time_to: [1220]
-
-    }
+    },
     {
-        name: "Diego Fernandes",
+        name: "Daniele Evangelista",
         avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
         whatsapp: "0000000",
-        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>
-    Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. 
-    Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
-    subject: "Química",
+        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        subject: "Química",
         cost: "20",
-        weekday: [0],
+        weekday: [1],
         time_from: [720],
         time_to: [1220]
-
-
-
     }
 ]
+
+function pageLanding(req, res) {
+    return res.render("index.html")
+}
+
+function pageStudy(req, res) {
+    return res.render("study.html")
+}
+
+function pageGiveClasses(req, res) {
+    return res.render("give-classes.html")
+}
 
 const express = require('express')
 const server = express()
 
-server.use(express.static("public"))
+//configurar nunjucks
+const nunjucks = require('nunjucks')
+nunjucks.configure('src/views',{
+    express: server,
+    noCache: true
+})
 
-    .get("/", (req, res) => {
-        return res.sendFile(__dirname + "/views/index.html")
-    })
-    .get("/study", (req, res) => {
-        return res.sendFile(__dirname + "/views/study.html")
-    })
-    .get("/give-classes", (req, res) => {
-        return res.sendFile(__dirname + "/views/give-classes.html")
-    })
-    .listen(5500)
+server
+//configurar arquivos estáticos (css, scripts, images)
+.use(express.static("public"))
+//rotas da aplicação
+.get("/", pageLanding)
+.get("/study", pageStudy)
+.get("/give-classes", pageGiveClasses)
+.listen(5500)
